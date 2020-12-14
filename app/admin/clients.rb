4c5fa0efd9867675aa5ed761b544f6c0d2270966
HungDo1291 <hung.dothixuan@gmail.com>
2020-12-15 02:37:29 +1100

    added permitted parameters to app/admin/clients.rb to avoid errors when creating new client on admin page

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Client do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :phone_number, :email, :birthday
+  permit_params :name, :phone_number, :email, :birthday
   #
   # or
   #
